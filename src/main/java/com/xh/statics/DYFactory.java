e73e8c833f77120147485d8c63855d8dbccce96e
NsATHUV <nsathuv@qq.com>
2024-12-16 11:31:29 +0800

    Bean不同的配置方法

---
+++ src/main/java/com/xh/statics/DYFactory.java
@@ -0,0 +1,2 @@
+package com.xh.statics;public class DYFactory {
+}
--- src/main/java/com/xh/test/StuTest.java
+++ src/main/java/com/xh/test/StuTest.java
@@ -4,7 +4,7 @@ import com.xh.dao.stuDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test {
+public class StuTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         stuDao dao = (stuDao) context.getBean("stuDao");
