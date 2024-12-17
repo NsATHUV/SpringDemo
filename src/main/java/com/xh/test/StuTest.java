8172a775c5c8b86cf5d2738be9570ecdcd853310
NsATHUV <nsathuv@qq.com>
2024-12-17 15:45:47 +0800

    测试JDBCTemplate

--- src/main/java/com/xh/test/StuTest.java
+++ src/main/java/com/xh/test/StuTest.java
@@ -1,9 +1,13 @@
 package com.xh.test;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.xh.dao.StuDao;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.beans.PropertyVetoException;
 
 public class StuTest {
     private static ApplicationContext getContext() {
@@ -30,6 +34,20 @@ public class StuTest {
         System.out.println(dao2);
         System.out.println(dao3);
         System.out.println(dao4);
+    }
+
+    @Test
+    //测试JDBCTemplate
+    public void testJDBC() throws PropertyVetoException {
+        ComboPooledDataSource ds = new ComboPooledDataSource();
+        ds.setDriverClass("com.mysql.cj.jdbc.Driver");
+        ds.setJdbcUrl("jdbc:mysql://localhost:3306/test");
+        ds.setUser("root");
+        ds.setPassword("123456");
+
+        //创建JDBCTemplate对象
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(ds);
+        jdbcTemplate.update("insert into students values(?,?,?)", 1, "张三", 18);
     }
 
 }
